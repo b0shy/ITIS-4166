36711b5bb3fb2cddefae3bfb0baa102e2a49c994
Bashar Shabani <boshyshabani@gmail.com>
2023-03-10 11:47:35 -0500

    Module 7 Initial Commit

---
+++ module7/module7/models/story.js
@@ -0,0 +1,40 @@
+const { DateTime } = require("luxon");
+const { v4: uuidv4 } = require('uuid');
+
+//need a referance variable to the stories collection in MongoDB
+let stories;
+
+exports.getCollection = db => {
+    stories = db.collection('stories');
+}
+exports.find = () => stories.find().toArray();
+
+exports.findById = id => stories.find(story => story.id === id);
+
+exports.save = function (story) {
+    story.id = uuidv4();
+    story.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
+    stories.push(story);
+};
+
+exports.updateById = function (id, newStory) {
+    let story = stories.find(story => story.id === id);
+    if (story) {
+        story.title = newStory.title;
+        story.content = newStory.content;
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+exports.deleteById = function (id) {
+    let index = stories.findIndex(story => story.id === id);
+    if (index !== -1) {
+        stories.splice(index, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
