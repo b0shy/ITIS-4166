10264fe620bd198d8a1f8f010f32928e211c9a5d
Bashar Shabani <boshyshabani@gmail.com>
2023-03-31 20:53:30 -0400

    commit #2

--- exercise8/ShabaniBasharExercise8/app.js
+++ exercise8/ShabaniBasharExercise8/app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const storyRoutes = require('./routes/storyRoutes');
-
 const User = require('./models/user');
 
 //create app
@@ -30,6 +31,19 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'secretpassword123',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    store: new MongoStore({ mongoUrl: 'mongodb://0.0.0.0:27017/demos' })
+}));
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    next();
+});
 
 //set up routes
 app.get('/', (req, res) => {
@@ -68,6 +82,7 @@ app.post('/users/login', (req, res, next) => {
                 user.comparePassword(password)
                     .then(result => {
                         if (result) {
+                            req.session.user = user._id; //store users id in the session
                             res.redirect('/users/profile');
                         } else {
                             console.log('wrong password');
@@ -81,6 +96,24 @@ app.post('/users/login', (req, res, next) => {
         })
         .catch(err => next(err));
 });
+
+//get profile
+app.get('/users/profile', (req, res, next) => {
+    let id = req.session.user;
+    User.findById(id)
+        .then(user => res.render('user/profile', { user }))
+        .catch(err => next(err));
+});
+
+//logout the user
+app.get('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+        if (err)
+            return next(err);
+        else
+            res.redirect('/');
+    })
+})
 
 app.use('/stories', storyRoutes);
 
