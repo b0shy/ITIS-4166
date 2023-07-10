940c4dd9dcd923c859dfc9f88d33cc805b5771c9
Bashar Shabani <boshyshabani@gmail.com>
2023-07-10 12:21:55 -0400

    Name Change

--- project5/ShabaniBasharProject5/middleware/validator.js
+++ project5/ShabaniBasharProject5/middleware/validator.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const { body } = require('express-validator');
+const validator = require('validator');
 
 exports.validateId = (req, res, next) => {
     let id = req.params.id;
-    if(id.match(/^[0-9a-fA-F]{24}$/)){
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
         return next();
     } else {
         let err = new Error('Invalid story id');
@@ -10,3 +12,87 @@ exports.validateId = (req, res, next) => {
         return next(err);
     }
 };
+
+exports.validateCreateEvent = [
+    body('title').trim().notEmpty(),
+    body('description').trim().notEmpty(),
+    body('location').trim().notEmpty(),
+    body('start').custom((value) => {
+        if (!validator.isISO8601(value)) {
+            throw new Error('Invalid date format');
+        }
+        const now = new Date();
+        const start = new Date(value);
+        if (start <= now) {
+            throw new Error('Event start date must be after current date');
+        }
+        return true;
+    }),
+    body('end').custom((value, { req }) => {
+        if (!validator.isISO8601(value)) {
+            throw new Error('Invalid date format');
+        }
+        const start = new Date(req.body.start);
+        const end = new Date(value);
+        if (end <= start) {
+            throw new Error('Event end date must be after start date');
+        }
+        return true;
+    }),
+    body('email').normalizeEmail(),
+    body('email').isEmail(),
+    body('category').isIn([
+        'Music',
+        'Art',
+        'Food',
+        'Sports',
+        'Technology',
+        'Film',
+        'Other',
+    ]),
+    body('rsvp').isIn(['YES', 'NO', 'MAYBE']),
+    body('password').optional().isLength({ min: 8, max: 64 }),
+    body('*').escape(),
+];
+
+exports.validateUpdateEvent = [
+    body('title').optional().trim().notEmpty(),
+    body('description').optional().trim().notEmpty(),
+    body('location').optional().trim().notEmpty(),
+    body('start').optional().custom((value) => {
+        if (!validator.isISO8601(value)) {
+            throw new Error('Invalid date format');
+        }
+        const now = new Date();
+        const start = new Date(value);
+        if (start <= now) {
+            throw new Error('Event start date must be after current date');
+        }
+        return true;
+    }),
+    body('end').optional().custom((value, { req }) => {
+        if (!validator.isISO8601(value)) {
+            throw new Error('Invalid date format');
+        }
+        const start = new Date(req.body.start);
+        const end = new Date(value);
+        if (end <= start) {
+            throw new Error('Event end date must be after start date');
+        }
+        return true;
+    }),
+    body('email').optional().normalizeEmail(),
+    body('email').optional().isEmail(),
+    body('category').optional().isIn([
+        'Music',
+        'Art',
+        'Food',
+        'Sports',
+        'Technology',
+        'Film',
+        'Other',
+    ]),
+    body('rsvp').optional().isIn(['YES', 'NO', 'MAYBE']),
+    body('password').optional().isLength({ min: 8, max: 64 }),
+    body('*').escape(),
+];
--- project5/ShabaniBasharProject5/routes/eventRoutes.js
+++ project5/ShabaniBasharProject5/routes/eventRoutes.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const controller = require('../controllers/eventController');
-const {fileUpload} = require('../middleware/fileUpload');
+const { fileUpload } = require('../middleware/fileUpload');
 const router = express.Router();
-const {isLoggedIn, isAuthor} = require('../middleware/auth');
-const {validateId} = require('../middleware/validator');
+const { isLoggedIn, isAuthor } = require('../middleware/auth');
+const { validateId, validateCreateEvent, validateUpdateEvent } = require('../middleware/validator');
 
-//GET /events: send all events to the user
-
+// GET /events: send all events to the user
 router.get('/', controller.index);
 
-//GET /events/new: send html form for creating a new event
-
+// GET /events/new: send html form for creating a new event
 router.get('/new', isLoggedIn, controller.new);
 
-//POST /events: create a new story
+// POST /events: create a new story
+router.post('/', isLoggedIn, validateCreateEvent, fileUpload, controller.create);
 
-router.post('/', isLoggedIn, fileUpload, controller.create);
-
-//GET /events/:id: send details of event identified by id
+// GET /events/:id: send details of event identified by id
 router.get('/:id', validateId, controller.show);
 
-//GET /events/:id/edit: send html form for editing an existing event
+// GET /events/:id/edit: send html form for editing an existing event
 router.get('/:id/edit', isLoggedIn, isAuthor, validateId, controller.edit);
 
-//PUT /events/:id: update the event identified by id
-router.put('/:id', isLoggedIn, isAuthor, validateId, fileUpload, controller.update);
+// PUT /events/:id: update the event identified by id
+router.put('/:id', isLoggedIn, isAuthor, validateId, validateUpdateEvent, fileUpload, controller.update);
 
-//DELETE /events/:id: delete the event identified by id
-router.delete('/:id', isLoggedIn, isAuthor, validateId, controller.delete);
+// DELETE /events/:id: delete the event identified by id
+router.delete('/:id', isLoggedIn, isAuthor, controller.delete);
 
 module.exports = router;
